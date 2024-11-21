9bbe402a6ef5c5be98b1de7a901fbcc4996ecae8
mac <mac@mbp-mac.home>
2024-11-21 17:42:24 +0100

    Made a translation of the buttons

--- src/components/ProcedureСard/TabButtons.js
+++ src/components/ProcedureСard/TabButtons.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';  
 import './TabButtons.css';
 
 const TabButtons = ({ activeTab, onTabClick }) => {
+  const { t } = useTranslation(); 
+
   return (
     <div className="button-container">
       <button
         className={`tab-button ${activeTab === 'depilacja' ? 'active' : ''}`}
         onClick={() => onTabClick('depilacja')}
       >
-        Depilacja
+        {t('depilation')}
       </button>
       <button
         className={`tab-button ${activeTab === 'masaż' ? 'active' : ''}`}
         onClick={() => onTabClick('masaż')}
       >
-        Masaż
+        {t('massage')} 
       </button>
       <button
         className={`tab-button ${activeTab === 'medycyna' ? 'active' : ''}`}
         onClick={() => onTabClick('medycyna')}
       >
-        Kosmetologia medyczna
+        {t('medical_cosmetology')}
       </button>
     </div>
   );
