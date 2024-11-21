9aad3e7524e7f843f98a81b27be940832415dfae
mac <mac@mbp-mac.home>
2024-11-21 17:38:16 +0100

    fixc component card

--- src/components/ProcedureСard/Card.js
+++ src/components/ProcedureСard/Card.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import './Card.css';
 
 const Card = ({ title, description, imgSrc, altText, id }) => {
+  const { t } = useTranslation();
+
   return (
     <div className="grid-item">
       <div className="overlay">
         <h3>{title}</h3>
         <p>{description}</p>
         <Link to={`/procedure/${id}`} className="open-modal-btn">➔</Link>
-        <a href="#contact" className="cta-button">Zapisać się</a>
+        <a href="#contact" className="cta-button">{t('sign_up')}</a> {/* Используем t для перевода */}
       </div>
       <img src={imgSrc} alt={altText} />
     </div>
