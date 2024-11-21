5af9ce7fce0b414d683baa5d5b5500a9e119a372
даник <danik@MacBook-Pro-danik.local>
2024-11-21 10:09:22 +0100

    cerat slider in AboutUs but fix foto

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,17 +1,59 @@
 import React from 'react';
+import Slider from 'react-slick';
 import './AboutUs.css'; 
 
-function AboutUs () {
+import image1 from '../../assets/obout.jpg';
+import image2 from '../../assets/obout2.png';
+import image3 from '../../assets/obout3.png';
+
+function AboutUs() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  };
+
   return (
     <main>
-    <section id="about" className="about">
-      <div className="container">
-        <div className="about-content">
-        </div>
-      </div>
-  </section>
-  </main>
+      <section id="about" className="about">
+        <Slider {...settings}>
+          <div>
+            <img src={image1} alt="Slide 1" className="slide-image" />
+          </div>
+          <div>
+            <img src={image2} alt="Slide 2" className="slide-image" />
+          </div>
+          <div>
+            <img src={image3} alt="Slide 3" className="slide-image" />
+          </div>
+        </Slider>
+      </section>
+    </main>
   );
 }
+
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-next" onClick={onClick}>
+      →
+    </div>
+  );
+};
+
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="slick-arrow slick-prev" onClick={onClick}>
+      ←
+    </div>
+  );
+};
 
 export default AboutUs;
